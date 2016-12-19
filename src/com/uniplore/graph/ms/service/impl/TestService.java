5b9a2620a1846215d074817c4eb644d29071efba
junpengzhu <13101900@qq.com>
2016-12-19 09:49:20 +0800

    给demo的service方法和spring.xml中分别增加了事务控制

--- src/com/uniplore/graph/ms/service/impl/TestService.java
+++ src/com/uniplore/graph/ms/service/impl/TestService.java
@@ -2,12 +2,14 @@ package com.uniplore.graph.ms.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.uniplore.graph.ms.dao.ITestUserDao;
 import com.uniplore.graph.ms.service.ITestService;
 
 @Service
+@Transactional
 public class TestService implements ITestService {
 
 	@Autowired 

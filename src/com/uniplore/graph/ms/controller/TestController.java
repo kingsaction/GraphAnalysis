3a1fcfcb53495619ef9619f88824ca0abd5883e8
junpengzhu <13101900@qq.com>
2016-12-20 15:48:27 +0800

    测试注释模板

--- src/com/uniplore/graph/ms/controller/TestController.java
+++ src/com/uniplore/graph/ms/controller/TestController.java
@@ -6,13 +6,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.uniplore.graph.ms.service.ITestService;
 
-
+/*
+ * 
+ * 版权所有  2016-ACMIS Lab  
+ * 项目名称  graphanalysis       
+ * 类描述  
+ * 类名称    com.uniplore.graph.ms.controller.TestController       
+ * 创建人    朱君鹏
+ * 创建时间  2016年12月20日 下午3:47:48     
+ * 修改人  
+ * 修改时间  2016年12月20日 下午3:47:48     
+ * 修改备注     
+ * @version  1.0
+ */
 @Controller
 public class TestController {
 
 	@Autowired
 	private ITestService testService;
 	
+	/**
+	 * 
+	 * @Title  test  
+	 * @Description TODO(这里用一句话描述这个方法的作用)  
+	 * @return
+	 */
 	@RequestMapping(value="/test")
 	public String test(){
 		testService.test();
@@ -21,8 +39,14 @@ public class TestController {
 		//return "forward:test1";    //页面转发，url不变，request可以共享
 	}
 	
+	/**
+	 * 
+	 * @Title  test1  
+	 * @Description TODO(这里用一句话描述这个方法的作用)  
+	 * @param name
+	 */
 	@RequestMapping(value="/test1")
-	public void test1() {
+	public void test1(String name) {
 		System.out.println("哈哈");
 	}
 }

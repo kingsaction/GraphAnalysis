8b2d67d8e3b203010fb7ee4bd47ad903ed6d8f8e
junpengzhu <13101900@qq.com>
2016-12-27 17:22:43 +0800

    修改全局异常处理模块，当发生异常时，不会抛出给用户，而是以邮件的形式发送给管理员，修复代码，目前项目还在开发阶段，所以注释了spring.xml中的异常处理bean，等项目上线之后取消注释

--- src/com/uniplore/graph/exception/HandlerException.java
+++ src/com/uniplore/graph/exception/HandlerException.java
@@ -1,10 +1,24 @@
 package com.uniplore.graph.exception;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Date;
+import java.util.Properties;
+
+import javax.mail.Address;
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.uniplore.graph.utils.email.GMailAuthenticator;
+import com.uniplore.graph.utils.email.SendEmail;
 
 public class HandlerException implements HandlerExceptionResolver {
 
@@ -20,6 +34,48 @@ public class HandlerException implements HandlerExceptionResolver {
 		}
 		
 		String message = customException.getMessage();   //获取到错误信息
+		StringWriter writer = new StringWriter();
+		customException.printStackTrace(new PrintWriter(writer, true));
+		String error = writer.toString();  //得到堆栈的错误信息
+		
+		try {
+	        String host = "smtp.qq.com";     //邮件服务器
+	        String from = "13101900@qq.com";   //发送邮件的QQ
+	        String authcode = "ecdudaqyndcibhag";  //对于QQ的个人邮箱而言，密码使用的是客户端的授权码，而不是用户的邮箱密码
+	        Properties props = System.getProperties();
+	        props.put("mail.smtp.host", host);
+	        props.setProperty("mail.transport.protocol", "smtp");  // 发送邮件协议名称
+	        props.put("mail.smtp.auth", "true");   //开启授权
+	        props.put("mail.smtp.user", from);
+	        props.put("mail.smtp.password", authcode);
+	        props.put("mail.smtp.port", "587");   //smtp邮件服务器的端口号，必须是587,465调试时失败
+	        props.setProperty("mail.smtp.ssl.enable", "true");
+	        props.setProperty("mail.smtp.connectiontimeout", "5000");
+	        
+	        Session session = Session.getDefaultInstance(props,new GMailAuthenticator("13101900@qq.com", "ecdudaqyndcibhag"));
+	       
+	        props.put("mail.debug", "true");
+	        
+	        MimeMessage emailMessage = new MimeMessage(session);
+	        Address fromAddress = new InternetAddress(from,"优联博睿");
+	        Address toAddress = new InternetAddress("13101900@qq.com");
+
+	        emailMessage.setFrom(fromAddress);
+	        emailMessage.setRecipient(Message.RecipientType.TO, toAddress);
+
+	        emailMessage.setSubject("报错信息");
+	        emailMessage.setSentDate(new Date());
+	        emailMessage.setContent(error,"text/html;charset=utf-8");
+	        Transport transport = session.getTransport();
+	        transport.connect(host, from, authcode);
+	        emailMessage.saveChanges();
+	        Transport.send(emailMessage);
+	        transport.close();
+
+	    }catch(Exception ex){
+	    	throw new RuntimeException(ex);
+	    }
+		
 		
 		ModelAndView modelAndView = new ModelAndView();
 		modelAndView.addObject("message", message);

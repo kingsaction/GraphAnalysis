2628080f17edf52390b933873011310ebb7294a3
junpengzhu <13101900@qq.com>
2017-01-17 22:08:30 +0800

    增加注释

--- src/com/uniplore/graph/dsm/common/controller/DispatcherMain.java
+++ src/com/uniplore/graph/dsm/common/controller/DispatcherMain.java
@@ -3,6 +3,11 @@ package com.uniplore.graph.dsm.common.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * 当成功登陆之后，转发到主页上
+ * @author 朱君鹏
+ *
+ */
 @Controller
 @RequestMapping(value="/")
 public class DispatcherMain {

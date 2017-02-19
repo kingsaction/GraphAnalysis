c531bbe27f4695197126686d2fe03230c8d4e7a0
junpengzhu <13101900@qq.com>
2017-02-19 21:33:38 +0800

    更新代码规范

--- src/com/uniplore/graph/common/entity/IPerson.java
+++ src/com/uniplore/graph/common/entity/IPerson.java
@@ -1,7 +1,6 @@
 package com.uniplore.graph.common.entity;
 
 /**
- * 
  * 版权所有  2016-ACMIS Lab  
  * 项目名称  graphanalysis       
  * 类描述    IPerson接口，所有在代码中出现的人(普通用户、管理员)都要实现该接口，该接口实际上没有任何的方法，只是为了使得所有用户均有发送邮件的公共方法 

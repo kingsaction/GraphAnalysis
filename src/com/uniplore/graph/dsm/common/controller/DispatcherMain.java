4595b03f3c35888b33b513e52bd36801f3d07c26
junpengzhu <13101900@qq.com>
2017-02-19 21:23:55 +0800

    checkStyle改写DispatcherMain.java代码，使之更加规范

--- src/com/uniplore/graph/dsm/common/controller/DispatcherMain.java
+++ src/com/uniplore/graph/dsm/common/controller/DispatcherMain.java
@@ -3,17 +3,16 @@ package com.uniplore.graph.dsm.common.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/**
- * 当成功登陆之后，转发到主页上
+/**  当成功登陆之后，转发到主页上.
  * @author 朱君鹏
  *
  */
 @Controller
-@RequestMapping(value="/")
+@RequestMapping(value = "/")
 public class DispatcherMain {
 
-	@RequestMapping(value="/dsm/main")
-	public String dispatcherMain(){
-		return "dsm/main";
-	}
+  @RequestMapping(value = "/dsm/main")
+  public String dispatcherMain() { 
+    return "dsm/main";
+  }
 }

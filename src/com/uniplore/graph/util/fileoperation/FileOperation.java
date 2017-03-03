72aea622990b6d4620dd8b5f504413b8a8e9baff
junpengzhu <13101900@qq.com>
2017-03-03 16:19:40 +0800

    checkStyle修改代码规范

--- src/com/uniplore/graph/util/fileoperation/FileOperation.java
+++ src/com/uniplore/graph/util/fileoperation/FileOperation.java
@@ -8,22 +8,22 @@ import java.io.Reader;
 import java.io.UnsupportedEncodingException;
 
 public class FileOperation {
-	 public static String readFileContent(InputStream in) { 
-         StringBuffer sb = new StringBuffer(); 
-         try { 
-                 Reader r = new InputStreamReader(in); 
-                 int length = 0; 
-                 for (char[] c = new char[1024]; (length = r.read(c)) != -1;) { 
-                         sb.append(c, 0, length); 
-                 } 
-                 r.close(); 
-         } catch (UnsupportedEncodingException e) { 
-                 e.printStackTrace(); 
-         } catch (FileNotFoundException e) { 
-                 e.printStackTrace(); 
-         } catch (IOException e) { 
-                 e.printStackTrace(); 
-         } 
-         return sb.toString(); 
- } 
+  public static String readFileContent(InputStream in) { 
+    StringBuffer sb = new StringBuffer(); 
+    try { 
+      Reader r = new InputStreamReader(in); 
+      int length = 0; 
+      for (char[] c = new char[1024]; (length = r.read(c)) != -1;) { 
+        sb.append(c, 0, length); 
+      } 
+      r.close(); 
+    } catch (UnsupportedEncodingException e) { 
+      e.printStackTrace(); 
+    } catch (FileNotFoundException e) { 
+      e.printStackTrace(); 
+    } catch (IOException e) { 
+      e.printStackTrace(); 
+    } 
+    return sb.toString(); 
+  } 
 }

4c9380a87a8a366e9e91c71546242cfc42b658a8
junpengzhu <13101900@qq.com>
2017-04-22 16:21:27 +0800

    成功的连接PostgreSQL、Greenplum、Oracle，但是展示页面需要进行大范围的修改

--- web/js/dsm/db/db.js
+++ web/js/dsm/db/db.js
@@ -1,12 +1,13 @@
 function modal_mysql_login(){
 	//首先获取到表单元素的所有值
-	var driverName = $("#driverName option:selected").text();   //获取到select选中的值
-	var dataBaseType = $("#dataBaseType").val();
-	var ipAddress = $("#ipAddress").val();
-	var portNumber = $("#portNumber").val();
-	var connectionName = $("#connectionName").val();
-	var userName = $("#userName").val();
-	var password = $("#password").val();
+	var driverName = $("#mysqlDriverName option:selected").text();   //获取到select选中的值
+	var dataBaseType = $("#mysqlDataBaseType").val();
+	var ipAddress = $("#mysqlIpAddress").val();
+	var portNumber = $("#mysqlPortNumber").val();
+	var connectionName = $("#mysqlConnectionName").val();
+	var dataBaseName = $("#mysqlDataBaseName").val();
+	var userName = $("#mysqlUserName").val();
+	var password = $("#mysqlPassword").val();
 	//输出上述的所有值，判断是是否正确的得到连接信息
 	//alert("驱动名称为:"+driverName+"数据库类型为:"+dataBaseType+"ip地址为:"+ipAddress+"端口号为:"+portNumber+"连接名为"+connectionName+"用户名为"+userName+"密码为:"+password);
 	$.ajax({
@@ -19,6 +20,211 @@ function modal_mysql_login(){
 			"ipAddress": ipAddress,
 			"portNumber": portNumber,
 			"connectionName": connectionName,
+			"dataBaseName": dataBaseName,
+			"userName": userName,
+			"password": password,
+		},
+		success: function(data) {
+				//alert("返回值为:"+data);   //判断是否成功的接收到返回值，当成功的连接数据源接收到的数据为:"数据库连接成功"
+				//连接数据源失败时，返回值为"数据库连接失败"
+			if("数据库连接成功" == data){
+				//跳转
+				$('#modal-db-json').closeModal({
+                    out_duration: 1000,    /*关闭模态框,1000代表1000ms，在1s内关闭模态框*/
+                });
+				var form = $("<form method='post'></form>");
+				/*参考文章: http://stackoverflow.com/questions/42053775/getting-error-form-submission-canceled-because-the-form-is-not-connected*/
+				$(document.body).append(form);    //动态构建的表单，必须要使用这个语句，将form加到body中才行
+				
+		        form.attr({"action":"/graphanalysis/dsm/db/dbPage"});
+		        var arr = new Array();
+		        arr["driverName"] = driverName;
+		        arr["dataBaseType"] = dataBaseType;
+		        arr["ipAddress"] = ipAddress;
+		        arr["portNumber"] = portNumber;
+		        arr["connectionName"] = connectionName;
+		        arr["userName"] = userName;
+		        arr["password"]= password;
+		        for (var key in arr)
+		        {
+		            var input = $("<input type='hidden'>");
+		            input.attr({"name":key});
+		            input.val(arr[key]);
+		            form.append(input);
+		        }
+		        form.submit();
+				//window.location.href = "/graphanalysis/dsm/db/dbPage";  //成功上传文件之后跳转到指定的路径中	
+		}else if("用户名和密码无效" == data){
+				swal("Oops...", "用户名和密码无效", "error");
+			}else if("与数据库通信时出错，不能连接到数据库服务器，请检查服务器是否正在运行以及您是否有权访问请求的数据库" == data){
+				swal("Oops...","与数据库通信时出错，不能连接到数据库服务器，请检查服务器是否正在运行以及您是否有权访问请求的数据库","error");
+			}else{
+				swal("Oops...","数据库连接失败","error");
+			} //if语句结束    
+			
+			}
+	})
+}
+
+function modal_postgresql_login(){
+	//首先获取到表单元素的所有值
+	var driverName = $("#postgresqlDriverName option:selected").text();   //获取到select选中的值
+	var dataBaseType = $("#postgresqlDataBaseType").val();
+	var ipAddress = $("#postgresqlIpAddress").val();
+	var portNumber = $("#postgresqlPortNumber").val();
+	var connectionName = $("#postgresqlConnectionName").val();
+	var dataBaseName = $("#postgresqlDataBaseName").val();
+	var userName = $("#postgresqlUserName").val();
+	var password = $("#postgresqlPassword").val();
+	//输出上述的所有值，判断是是否正确的得到连接信息
+	//alert("驱动名称为:"+driverName+"数据库类型为:"+dataBaseType+"ip地址为:"+ipAddress+"端口号为:"+portNumber+"连接名为"+connectionName+"用户名为"+userName+"密码为:"+password);
+	$.ajax({
+		url: "/graphanalysis/dsm/db/connection?t=" + (new Date()).getTime(),   //提交数据，测试是否能够成功的连接数据源,url路径采用绝对路径
+		type:"POST",
+		data: {
+			//成功的连接数据源之后要能获取到数据源的信息
+			"driverName": driverName,
+			"dataBaseType": dataBaseType,
+			"ipAddress": ipAddress,
+			"portNumber": portNumber,
+			"connectionName": connectionName,
+			"dataBaseName": dataBaseName,
+			"userName": userName,
+			"password": password,
+		},
+		success: function(data) {
+				//alert("返回值为:"+data);   //判断是否成功的接收到返回值，当成功的连接数据源接收到的数据为:"数据库连接成功"
+				//连接数据源失败时，返回值为"数据库连接失败"
+			if("数据库连接成功" == data){
+				//跳转
+				$('#modal-db-json').closeModal({
+                    out_duration: 1000,    /*关闭模态框,1000代表1000ms，在1s内关闭模态框*/
+                });
+				var form = $("<form method='post'></form>");
+				/*参考文章: http://stackoverflow.com/questions/42053775/getting-error-form-submission-canceled-because-the-form-is-not-connected*/
+				$(document.body).append(form);    //动态构建的表单，必须要使用这个语句，将form加到body中才行
+				
+		        form.attr({"action":"/graphanalysis/dsm/db/dbPage"});
+		        var arr = new Array();
+		        arr["driverName"] = driverName;
+		        arr["dataBaseType"] = dataBaseType;
+		        arr["ipAddress"] = ipAddress;
+		        arr["portNumber"] = portNumber;
+		        arr["connectionName"] = connectionName;
+		        arr["userName"] = userName;
+		        arr["password"]= password;
+		        for (var key in arr)
+		        {
+		            var input = $("<input type='hidden'>");
+		            input.attr({"name":key});
+		            input.val(arr[key]);
+		            form.append(input);
+		        }
+		        form.submit();
+				//window.location.href = "/graphanalysis/dsm/db/dbPage";  //成功上传文件之后跳转到指定的路径中	
+		}else if("用户名和密码无效" == data){
+				swal("Oops...", "用户名和密码无效", "error");
+			}else if("与数据库通信时出错，不能连接到数据库服务器，请检查服务器是否正在运行以及您是否有权访问请求的数据库" == data){
+				swal("Oops...","与数据库通信时出错，不能连接到数据库服务器，请检查服务器是否正在运行以及您是否有权访问请求的数据库","error");
+			}else{
+				swal("Oops...","数据库连接失败","error");
+			} //if语句结束    
+			
+			}
+	})
+}
+
+function modal_greenplum_login(){
+	//首先获取到表单元素的所有值
+	var driverName = $("#greenplumDriverName option:selected").text();   //获取到select选中的值
+	var dataBaseType = $("#greenplumDataBaseType").val();
+	var ipAddress = $("#greenplumIpAddress").val();
+	var portNumber = $("#greenplumPortNumber").val();
+	var connectionName = $("#greenplumConnectionName").val();
+	var dataBaseName = $("#greenplumDataBaseName").val();
+	var userName = $("#greenplumUserName").val();
+	var password = $("#greenplumPassword").val();
+	//输出上述的所有值，判断是是否正确的得到连接信息
+	//alert("驱动名称为:"+driverName+"数据库类型为:"+dataBaseType+"ip地址为:"+ipAddress+"端口号为:"+portNumber+"连接名为"+connectionName+"用户名为"+userName+"密码为:"+password);
+	$.ajax({
+		url: "/graphanalysis/dsm/db/connection?t=" + (new Date()).getTime(),   //提交数据，测试是否能够成功的连接数据源,url路径采用绝对路径
+		type:"POST",
+		data: {
+			//成功的连接数据源之后要能获取到数据源的信息
+			"driverName": driverName,
+			"dataBaseType": dataBaseType,
+			"ipAddress": ipAddress,
+			"portNumber": portNumber,
+			"connectionName": connectionName,
+			"dataBaseName": dataBaseName,
+			"userName": userName,
+			"password": password,
+		},
+		success: function(data) {
+				//alert("返回值为:"+data);   //判断是否成功的接收到返回值，当成功的连接数据源接收到的数据为:"数据库连接成功"
+				//连接数据源失败时，返回值为"数据库连接失败"
+			if("数据库连接成功" == data){
+				//跳转
+				$('#modal-db-json').closeModal({
+                    out_duration: 1000,    /*关闭模态框,1000代表1000ms，在1s内关闭模态框*/
+                });
+				var form = $("<form method='post'></form>");
+				/*参考文章: http://stackoverflow.com/questions/42053775/getting-error-form-submission-canceled-because-the-form-is-not-connected*/
+				$(document.body).append(form);    //动态构建的表单，必须要使用这个语句，将form加到body中才行
+				
+		        form.attr({"action":"/graphanalysis/dsm/db/dbPage"});
+		        var arr = new Array();
+		        arr["driverName"] = driverName;
+		        arr["dataBaseType"] = dataBaseType;
+		        arr["ipAddress"] = ipAddress;
+		        arr["portNumber"] = portNumber;
+		        arr["connectionName"] = connectionName;
+		        arr["userName"] = userName;
+		        arr["password"]= password;
+		        for (var key in arr)
+		        {
+		            var input = $("<input type='hidden'>");
+		            input.attr({"name":key});
+		            input.val(arr[key]);
+		            form.append(input);
+		        }
+		        form.submit();
+				//window.location.href = "/graphanalysis/dsm/db/dbPage";  //成功上传文件之后跳转到指定的路径中	
+		}else if("用户名和密码无效" == data){
+				swal("Oops...", "用户名和密码无效", "error");
+			}else if("与数据库通信时出错，不能连接到数据库服务器，请检查服务器是否正在运行以及您是否有权访问请求的数据库" == data){
+				swal("Oops...","与数据库通信时出错，不能连接到数据库服务器，请检查服务器是否正在运行以及您是否有权访问请求的数据库","error");
+			}else{
+				swal("Oops...","数据库连接失败","error");
+			} //if语句结束    
+			
+			}
+	})
+}
+
+function modal_oracle_login(){
+	//首先获取到表单元素的所有值
+	var driverName = $("#oracleDriverName option:selected").text();   //获取到select选中的值
+	var dataBaseType = $("#oracleDataBaseType").val();
+	var ipAddress = $("#oracleIpAddress").val();
+	var portNumber = $("#oraclePortNumber").val();
+	var connectionName = $("#oracleConnectionName").val();
+	var dataBaseName = $("#oracleDataBaseName").val();
+	var userName = $("#oracleUserName").val();
+	var password = $("#oraclePassword").val();
+	//输出上述的所有值，判断是是否正确的得到连接信息
+	//alert("驱动名称为:"+driverName+"数据库类型为:"+dataBaseType+"ip地址为:"+ipAddress+"端口号为:"+portNumber+"连接名为"+connectionName+"用户名为"+userName+"密码为:"+password);
+	$.ajax({
+		url: "/graphanalysis/dsm/db/connection?t=" + (new Date()).getTime(),   //提交数据，测试是否能够成功的连接数据源,url路径采用绝对路径
+		type:"POST",
+		data: {
+			//成功的连接数据源之后要能获取到数据源的信息
+			"driverName": driverName,
+			"dataBaseType": dataBaseType,
+			"ipAddress": ipAddress,
+			"portNumber": portNumber,
+			"connectionName": connectionName,
+			"dataBaseName": dataBaseName,
 			"userName": userName,
 			"password": password,
 		},
--- web/js/dsm/main.js
+++ web/js/dsm/main.js
@@ -13,13 +13,22 @@ $(document).ready(function() {
 //获取到所有的参数信息，并将其传至后台测试能否连接数据源
 function modal_mysql_test(){
 	//首先获取到表单元素的所有值
-	var driverName = $("#driverName option:selected").text();   //获取到select选中的值
-	var dataBaseType = $("#dataBaseType").val();
-	var ipAddress = $("#ipAddress").val();
-	var portNumber = $("#portNumber").val();
-	var connectionName = $("#connectionName").val();
-	var userName = $("#userName").val();
-	var password = $("#password").val();
+	var driverName = $("#mysqlDriverName option:selected").text();   //获取到select选中的值
+	//alert("驱动为:" + driverName)
+	var dataBaseType = $("#mysqlDataBaseType").val();
+	//alert("数据库类型为:" + dataBaseType)
+	var ipAddress = $("#mysqlIpAddress").val();
+	//alert("ip地址为:" + ipAddress)
+	var portNumber = $("#mysqlPortNumber").val();
+	//alert("端口号为:" + portNumber)
+	var connectionName = $("#mysqlConnectionName").val();
+	//alert("连接名为:" + connectionName)
+	var dataBaseName = $("#mysqlDataBaseName").val();
+	//alert("数据库名为:" + dataBaseName)
+	var userName = $("#mysqlUserName").val();
+	//alert("用户名为:" + userName)
+	var password = $("#mysqlPassword").val();
+	//alert("密码为:" + password)
 	//输出上述的所有值，判断是是否正确的得到连接信息
 	//alert("驱动名称为:"+driverName+"数据库类型为:"+dataBaseType+"ip地址为:"+ipAddress+"端口号为:"+portNumber+"连接名为"+connectionName+"用户名为"+userName+"密码为:"+password);
 	$.ajax({
@@ -33,6 +42,7 @@ function modal_mysql_test(){
 			"ipAddress": ipAddress,
 			"portNumber": portNumber,
 			"connectionName": connectionName,
+			"dataBaseName": dataBaseName,
 			"userName": userName,
 			"password": password,
 		},
@@ -51,17 +61,179 @@ function modal_mysql_test(){
 			
 			}
 	})
-	
-	/*$('#loader').shCircleLoader();*/
 }
 
+function modal_postgresql_test(){
+	//首先获取到表单元素的所有值
+	var driverName = $("#postgresqlDriverName option:selected").text();   //获取到select选中的值
+	//alert("驱动为:" + driverName)
+	var dataBaseType = $("#postgresqlDataBaseType").val();
+	//alert("数据库类型为:" + dataBaseType)
+	var ipAddress = $("#postgresqlIpAddress").val();
+	//alert("ip地址为:" + ipAddress)
+	var portNumber = $("#postgresqlPortNumber").val();
+	//alert("端口号为:" + portNumber)
+	var connectionName = $("#postgresqlConnectionName").val();
+	//alert("连接名为:" + connectionName)
+	var dataBaseName = $("#postgresqlDataBaseName").val();
+	//alert("数据库名为:" + dataBaseName)
+	var userName = $("#postgresqlUserName").val();
+	//alert("用户名为:" + userName)
+	var password = $("#postgresqlPassword").val();
+	//alert("密码为:" + password)
+	//输出上述的所有值，判断是是否正确的得到连接信息
+	//alert("驱动名称为:"+driverName+"数据库类型为:"+dataBaseType+"ip地址为:"+ipAddress+"端口号为:"+portNumber+"连接名为"+connectionName+"用户名为"+userName+"密码为:"+password);
+	$.ajax({
+		async: false,  //设置ajax交互为同步方式，默认采用的是异步方式
+		url: "/graphanalysis/dsm/db/connection?t=" + (new Date()).getTime(),   //提交数据，测试是否能够成功的连接数据源,url路径采用绝对路径
+		type:"POST",
+		data: {
+			//成功的连接数据源之后要能获取到数据源的信息
+			"driverName": driverName,
+			"dataBaseType": dataBaseType,
+			"ipAddress": ipAddress,
+			"portNumber": portNumber,
+			"connectionName": connectionName,
+			"dataBaseName": dataBaseName,
+			"userName": userName,
+			"password": password,
+		},
+		success: function(data) {
+				//alert("返回值为:"+data);   //判断是否成功的接收到返回值，当成功的连接数据源接收到的数据为:"数据库连接成功"
+				//连接数据源失败时，返回值为"数据库连接失败"
+			if("数据库连接成功" == data){
+				swal("Good job!", "数据库连接成功", "success");
+			}else if("用户名和密码无效" == data){
+				swal("Oops...", "用户名和密码无效", "error");
+			}else if("与数据库通信时出错，不能连接到数据库服务器，请检查服务器是否正在运行以及您是否有权访问请求的数据库" == data){
+				swal("Oops...","与数据库通信时出错，不能连接到数据库服务器，请检查服务器是否正在运行以及您是否有权访问请求的数据库","error");
+			}else{
+				swal("Oops...","数据库连接失败","error");
+			} //if语句结束    
+			
+			}
+	})
+}
+
+function modal_greenplum_test(){
+	//首先获取到表单元素的所有值
+	var driverName = $("#greenplumDriverName option:selected").text();   //获取到select选中的值
+    //alert("驱动为:" + driverName)
+	var dataBaseType = $("#greenplumDataBaseType").val();
+	//alert("数据库类型为:" + dataBaseType)
+	var ipAddress = $("#greenplumIpAddress").val();
+	//alert("ip地址为:" + ipAddress)
+	var portNumber = $("#greenplumPortNumber").val();
+	//alert("端口号为:" + portNumber)
+	var connectionName = $("#greenplumConnectionName").val();
+	//alert("连接名为:" + connectionName)
+	var dataBaseName = $("#greenplumDataBaseName").val();
+	//alert("数据库名为:" + dataBaseName)
+	var userName = $("#greenplumUserName").val();
+	//alert("用户名为:" + userName)
+	var password = $("#greenplumPassword").val();
+	//alert("密码为:" + password)
+	//输出上述的所有值，判断是是否正确的得到连接信息
+	//alert("驱动名称为:"+driverName+"数据库类型为:"+dataBaseType+"ip地址为:"+ipAddress+"端口号为:"+portNumber+"连接名为"+connectionName+"用户名为"+userName+"密码为:"+password);
+	$.ajax({
+		async: false,  //设置ajax交互为同步方式，默认采用的是异步方式
+		url: "/graphanalysis/dsm/db/connection?t=" + (new Date()).getTime(),   //提交数据，测试是否能够成功的连接数据源,url路径采用绝对路径
+		type:"POST",
+		data: {
+			//成功的连接数据源之后要能获取到数据源的信息
+			"driverName": driverName,
+			"dataBaseType": dataBaseType,
+			"ipAddress": ipAddress,
+			"portNumber": portNumber,
+			"connectionName": connectionName,
+			"dataBaseName": dataBaseName,
+			"userName": userName,
+			"password": password,
+		},
+		success: function(data) {
+				//alert("返回值为:"+data);   //判断是否成功的接收到返回值，当成功的连接数据源接收到的数据为:"数据库连接成功"
+				//连接数据源失败时，返回值为"数据库连接失败"
+			if("数据库连接成功" == data){
+				swal("Good job!", "数据库连接成功", "success");
+			}else if("用户名和密码无效" == data){
+				swal("Oops...", "用户名和密码无效", "error");
+			}else if("与数据库通信时出错，不能连接到数据库服务器，请检查服务器是否正在运行以及您是否有权访问请求的数据库" == data){
+				swal("Oops...","与数据库通信时出错，不能连接到数据库服务器，请检查服务器是否正在运行以及您是否有权访问请求的数据库","error");
+			}else{
+				swal("Oops...","数据库连接失败","error");
+			} //if语句结束    
+			
+			}
+	})
+}
+
+function modal_oracle_test(){
+	//首先获取到表单元素的所有值
+	var driverName = $("#oracleDriverName option:selected").text();   //获取到select选中的值
+    //alert("驱动为:" + driverName)
+	var dataBaseType = $("#oracleDataBaseType").val();
+	//alert("数据库类型为:" + dataBaseType)
+	var ipAddress = $("#oracleIpAddress").val();
+	//alert("ip地址为:" + ipAddress)
+	var portNumber = $("#oraclePortNumber").val();
+	//alert("端口号为:" + portNumber)
+	var connectionName = $("#oracleConnectionName").val();
+	//alert("连接名为:" + connectionName)
+	var dataBaseName = $("#oracleDataBaseName").val();
+	//alert("数据库名为:" + dataBaseName)
+	var userName = $("#oracleUserName").val();
+	//alert("用户名为:" + userName)
+	var password = $("#oraclePassword").val();
+	//alert("密码为:" + password)
+	//输出上述的所有值，判断是是否正确的得到连接信息
+	//alert("驱动名称为:"+driverName+"数据库类型为:"+dataBaseType+"ip地址为:"+ipAddress+"端口号为:"+portNumber+"连接名为"+connectionName+"用户名为"+userName+"密码为:"+password);
+	$.ajax({
+		async: false,  //设置ajax交互为同步方式，默认采用的是异步方式
+		url: "/graphanalysis/dsm/db/connection?t=" + (new Date()).getTime(),   //提交数据，测试是否能够成功的连接数据源,url路径采用绝对路径
+		type:"POST",
+		data: {
+			//成功的连接数据源之后要能获取到数据源的信息
+			"driverName": driverName,
+			"dataBaseType": dataBaseType,
+			"ipAddress": ipAddress,
+			"portNumber": portNumber,
+			"connectionName": connectionName,
+			"dataBaseName": dataBaseName,
+			"userName": userName,
+			"password": password,
+		},
+		success: function(data) {
+				//alert("返回值为:"+data);   //判断是否成功的接收到返回值，当成功的连接数据源接收到的数据为:"数据库连接成功"
+				//连接数据源失败时，返回值为"数据库连接失败"
+			if("数据库连接成功" == data){
+				swal("Good job!", "数据库连接成功", "success");
+			}else if("用户名和密码无效" == data){
+				swal("Oops...", "用户名和密码无效", "error");
+			}else if("与数据库通信时出错，不能连接到数据库服务器，请检查服务器是否正在运行以及您是否有权访问请求的数据库" == data){
+				swal("Oops...","与数据库通信时出错，不能连接到数据库服务器，请检查服务器是否正在运行以及您是否有权访问请求的数据库","error");
+			}else{
+				swal("Oops...","数据库连接失败","error");
+			} //if语句结束    
+			
+			}
+	})
+}
 function readonly_mysql(){
-	$("#dataBaseType").removeClass("invalid");
-	$("#dataBaseType").addClass("valid");
+	$("#mysqlDataBaseType").removeClass("invalid");
+	$("#mysqlDataBaseType").addClass("valid");
 }
 
-function driver_validate(){
-	$(".select-dropdown").removeClass("invalid");
-	$(".select-dropdown").addClass("validate");
-	$(".select-dropdown").addClass("valid");
+function readonly_postgresql(){
+	$("#postgresqlDataBaseType").removeClass("invalid");
+	$("#postgresqlDataBaseType").addClass("valid");
+}
+
+function readonly_greenplum(){
+	$("#greenplumDataBaseType").removeClass("invalid");
+	$("#greenplumDataBaseType").addClass("valid");
+}
+
+function readonly_oracle(){
+	$("#greenplumDataBaseType").removeClass("invalid");
+	$("#greenplumDataBaseType").addClass("valid");
 }

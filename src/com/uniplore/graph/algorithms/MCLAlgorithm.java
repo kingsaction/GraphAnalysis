7c9b8d519d3f4b780a101ba05ea521c873db57cc
zhujunpengguizhou <13101900@qq.com>
2017-08-07 15:14:30 +0800

    实现这两个算法

---
+++ src/com/uniplore/graph/algorithms/MCLAlgorithm.java
@@ -0,0 +1,27 @@
+/**    
+* @Title         MCLAlgorithm.java  
+* @Package       com.uniplore.graph.algorithms  
+* @Description   TODO(用一句话描述该文件做什么)  
+* @author        朱君鹏     
+* @date          2017年8月7日 下午3:12:49  
+* @version       1.0    
+*/ 
+package com.uniplore.graph.algorithms;
+
+  
+/**     
+ * 版权所有  2017-ACMIS Lab  
+ * 项目名称  graphanalysis       
+ * 类描述  
+ * 类名称    com.uniplore.graph.algorithms.MCLAlgorithm       
+ * 创建人    朱君鹏
+ * 创建时间  2017年8月7日 下午3:12:49     
+ * 修改人  
+ * 修改时间  2017年8月7日 下午3:12:49     
+ * 修改备注     
+ * @version  1.0      
+ */
+
+public class MCLAlgorithm {
+
+}
---
+++ src/com/uniplore/graph/algorithms/SpectralAlgorithm.java
@@ -0,0 +1,27 @@
+/**    
+* @Title         SpectralAlgorithm.java  
+* @Package       com.uniplore.graph.algorithms  
+* @Description   TODO(用一句话描述该文件做什么)  
+* @author        朱君鹏     
+* @date          2017年8月7日 下午3:13:22  
+* @version       1.0    
+*/ 
+package com.uniplore.graph.algorithms;
+
+  
+/**     
+ * 版权所有  2017-ACMIS Lab  
+ * 项目名称  graphanalysis       
+ * 类描述  
+ * 类名称    com.uniplore.graph.algorithms.SpectralAlgorithm       
+ * 创建人    朱君鹏
+ * 创建时间  2017年8月7日 下午3:13:22     
+ * 修改人  
+ * 修改时间  2017年8月7日 下午3:13:22     
+ * 修改备注     
+ * @version  1.0      
+ */
+
+public class SpectralAlgorithm {
+
+}
